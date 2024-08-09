e03fce0bfbb46604c856aaaca104dbb94f96ab4a
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-09 08:47:44 +0530

    Deriving cart items in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import logoImage from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function Header() {
+  const { items } = useContext(CartContext);
+  const totalCartItems = items.reduce((total, item) => {
+    return total + item.quantity;
+  }, 0);
   return (
     <header id="main-header">
       <div id="title">
@@ -9,7 +15,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly={true}>Cart(0)</Button>
+        <Button textOnly={true}>Cart({totalCartItems})</Button>
       </nav>
     </header>
   );
