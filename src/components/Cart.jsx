b89241746402f7524a1cd8fdf26c63740cc949d4
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-10 18:07:04 +0530

    Added Component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import CartContext from "../store/CartContext";
+import Modal from "./UI/Modal";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+
+export default function Cart() {
+  const { items } = useContext(CartContext);
+  const cartTotal = items.reduce((total, item) => {
+    total + item.quantity * item.price;
+  }, 0);
+  return (
+    <Modal className="cart">
+      <h2>Your Cart</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly={true}>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
