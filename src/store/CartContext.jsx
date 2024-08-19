e4917244f73e9fd3edcc004bddc1c1f9b65fd0b2
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-19 14:14:38 +0530

    Added clearCart to store

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -5,6 +5,7 @@ const CartContext = createContext({
   items: [],
   addItem: () => {},
   removeItem: () => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -46,6 +47,10 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
+
   return state;
 }
 export function CartContextProvider({ children }) {
@@ -59,10 +64,15 @@ export function CartContextProvider({ children }) {
   function removeItem(id) {
     dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
+
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
   const cartContextValue = {
     items: cart.items,
     removeItem,
     addItem,
+    clearCart,
   };
 
   return (
