8e3beadb841efc5243715373cbbbbb336e689ae3
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-20 23:04:10 +0530

    Handled modal close operation

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({ children, open, ref, className = "" }) {
+export default function Modal({
+  children,
+  open,
+  ref,
+  className = "",
+  onClose,
+}) {
   const dialog = useRef();
   useEffect(() => {
+    const modal = dialog.current;
     if (open) {
-      dialog.current.showModal();
+      modal.showModal();
     }
+
+    return () => modal.close();
   }, [open]);
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`}>
+    <dialog ref={dialog} className={`modal ${className}`} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
