1d5918834c485255937b9156bc169faa9190273f
Amanpreet Singh <amanpreetsinghlubana@gmail.com>
2024-08-20 23:04:40 +0530

    Showing cart based on user progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ import { useContext } from "react";
 import logoImage from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import { UserProgressContext } from "../store/UserProgressContext";
 
 export default function Header() {
   const { items } = useContext(CartContext);
   const totalCartItems = items.reduce((total, item) => {
     return total + item.quantity;
   }, 0);
+  const { showCart } = useContext(UserProgressContext);
+
+  function handleShowCart() {
+    showCart();
+  }
   return (
     <header id="main-header">
       <div id="title">
@@ -15,7 +21,9 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly={true}>Cart({totalCartItems})</Button>
+        <Button textOnly={true} onClick={handleShowCart}>
+          Cart({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
